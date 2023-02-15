bd92e3895a0f18caddc30b77bbda8e33e76b9337
JaypeeLan <jplaniran01@gmail.com>
2023-02-15 15:49:26 +0100

    refactored logic

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+// ----------------------
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
 // Set attribute function
 const setAttributes = (elem, attr) => {
   for (const key in attr) {
@@ -8,17 +14,32 @@ const setAttributes = (elem, attr) => {
   }
 };
 
-// ----------------------
-let photosArray = [];
-
-// -------
-const count = 10;
+// variables for fetching data
+let count = 5;
 let apiKey = "atZlCWXnhMOyzUOltcZZAcFy1Spt5yloxIKg5XywCRo";
+let apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+// -------check if all images were loaded
+const imageLoaded = () => {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    // hide loader when images are completely loaded
+    loader.hidden = true;
+
+    count = 30;
+
+    apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  }
+};
+// ----------------------------------
 
 // create elements for DOM
 const displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // --------------------------------------
   photosArray.forEach((photo) => {
     // create link to the photo unsplash photo
     const link = document.createElement("a");
@@ -37,13 +58,16 @@ const displayPhotos = () => {
       title: photo.alt_description,
     });
 
+    //when laoding is completed
+    img.addEventListener("load", imageLoaded);
+
     // put both in image container
     link.appendChild(img);
     imageContainer.appendChild(link);
   });
 };
 
-// Get photos
+// fetch photos
 const getPhotos = async () => {
   try {
     const res = await fetch(apiURL);
@@ -53,8 +77,18 @@ const getPhotos = async () => {
 };
 
 // Load more photos when close to the end of the page
+//  innerHeight - height of the browser window (vh)
+//  window.scrollY - how far we have scrolled
+// document.body.offsetHeight - total height of what is on the page
+
 window.addEventListener("scroll", () => {
-  console.log("scrolled");
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
 });
 
 getPhotos();
